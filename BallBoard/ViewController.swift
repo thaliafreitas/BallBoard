d083c106e58d74616bf299af74c87f2321d3e8fe
thaliafreitas <freitas.thalia97@gmail.com>
2019-06-13 08:23:29 -0300

    Initial Commit

---
+++ BallBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BallBoard
+//
+//  Created by Thalia Freitas on 13/06/19.
+//  Copyright © 2019 Thalia Freitas. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
